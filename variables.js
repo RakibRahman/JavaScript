075366bbd9764c2ffbfbc2c666ebea1aa23fa28c
RakibRahman <md.rr.talukder@gmail.com>
2021-02-09 21:42:16 +0600

    Variables

---
+++ variables.js
@@ -0,0 +1,22 @@
+// var,let,const - three keywords to declare variables
+// variable name starts with letters,numbers,$,_
+//Can not start with numbers e.g., 1name
+
+let catAge = 8;
+let catAge2 = catAge * 3;
+console.log(catAge);
+console.log(catAge2);
+
+var $tk = 256;
+console.log($tk, "TK");
+
+const fourth = "Minato Namikaze"; //can not reassign variable value
+//const fourth = "Mo Namikaze";
+
+console.log(fourth);
+
+const daysInWeek = 7;
+console.log(daysInWeek);
+
+let eggCount = 42;
+console.log(eggCount + 2);
