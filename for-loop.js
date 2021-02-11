b7dc968438c78449a62979bc20306ed6b76335e9
RakibRahman <md.rr.talukder@gmail.com>
2021-02-11 17:13:42 +0600

    For Loop

---
+++ for-loop.js
@@ -0,0 +1,14 @@
+// let i = 1;
+// for (i; i <= 10; i += 2) {
+//   console.log(i);
+// }
+
+// let j = 1;
+// for (j; j <= 20; j++) {
+//   console.log(j * j);
+// }
+
+let k = 50;
+for (k; k >= 40; k--) {
+  console.log(k);
+}
