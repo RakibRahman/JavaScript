0264ba36754f4889528e41c6a79a72637d2f10b1
RakibRahman <md.rr.talukder@gmail.com>
2021-02-11 23:57:28 +0600

    Code Updated

--- exercises/simple-guess-game/script.js
+++ exercises/simple-guess-game/script.js
@@ -1,13 +1,27 @@
-let rightNumber = Math.floor(Math.random() * 10 + 1);
-let guess = parseInt(prompt("Enter the number between 1 to 10:"));
+function startGame() {
+  let rightNumber = generateRandom();
+  let guess = getInput();
 
-while (guess !== rightNumber) {
-  if (guess > rightNumber) {
-    //see output on console
-    console.log(`${guess} is too high!`);
-  } else {
-    console.log(`${guess} is too low!`);
+  while (guess !== rightNumber) {
+    if (guess > rightNumber) {
+      //see output on console
+      console.log(`${guess} is too high!`);
+    } else {
+      console.log(`${guess} is too low!`);
+    }
+    guess = getInput();
   }
-  guess = parseInt(prompt("Please Guess Again"));
+  alert("Correct Guess");
 }
-alert("Correct Guess");
+function generateRandom() {
+  return Math.floor(Math.random() * 10 + 1);
+}
+function getInput() {
+  let guess = parseInt(prompt("Enter the number between 1 to 10:"));
+  while (Number.isNaN(guess)) {
+    let guess = parseInt(prompt("Plz Enter a valid number"));
+  }
+  return guess;
+}
+
+startGame();
