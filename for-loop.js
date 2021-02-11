454b5f979a30ed6cf422402e62d01da513d2fc2b
RakibRahman <md.rr.talukder@gmail.com>
2021-02-11 18:34:29 +0600

    For Loop

--- for-loop.js
+++ for-loop.js
@@ -8,7 +8,20 @@
 //   console.log(j * j);
 // }
 
-let k = 50;
-for (k; k >= 40; k--) {
-  console.log(k);
+// let k = 50;
+// for (k; k >= 40; k--) {
+//   console.log(k);
+// }
+
+//Example
+const numbers = [19.2, 25.3, 49, 71, 6, 10, 11.9];
+let count = numbers.length;
+console.log("number:", count);
+
+let total = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  total += numbers[i];
+  console.log(numbers[i]);
 }
+console.log(`Total is: $${total} `);
