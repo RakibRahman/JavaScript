b2ec3a4394363c8c73770715e0eb82b1eb978fbd
RakibRahman <md.rr.talukder@gmail.com>
2021-02-12 19:13:53 +0600

    Objects

---
+++ objects-v2.js
@@ -0,0 +1,24 @@
+var person = {
+  username: "Rakib",
+  age: 21,
+  married: false,
+  dateOfBirth: "31/12/96",
+};
+console.log(person);
+console.log(person["username"]);
+console.log(person.age);
+
+console.log(Object.values(person)); //get values only
+
+//Object inside object
+
+var user = {
+  name: "Killer",
+  ID: 49494,
+  info: {
+    vip: true,
+    rank: "gold",
+  },
+};
+console.log(user.info);
+console.log(Object.values(user));
