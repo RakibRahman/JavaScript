f18c80e69c12dae43cff38b5d925d5fc29128c6f
RakibRahman <md.rr.talukder@gmail.com>
2021-02-12 22:44:24 +0600

    For Loop

---
+++ for-loop-v2.js
@@ -0,0 +1,11 @@
+var persons = [
+  { name: "Ash", age: 21 },
+  { name: "Sasuke", age: 91 },
+  { name: "Yohan", age: 54 },
+  { name: "Yahiko", age: 74 },
+];
+for (var i = 0; i < persons.length; i++) {
+  console.log(persons[i].name);
+  console.log(persons[i].age);
+  console.log("---------------");
+}
