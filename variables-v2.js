6c0b791d921c1775d8b265996cfc087bd5ee6fc1
RakibRahman <md.rr.talukder@gmail.com>
2021-02-12 18:52:21 +0600

    Variables

---
+++ variables-v2.js
@@ -0,0 +1,12 @@
+var userName = "Rakib";
+var age = 21;
+var married = false;
+var empty = undefined;
+
+console.log(userName, age, married, empty);
+
+//typeof
+console.log(userName, " Type: " + typeof userName);
+console.log(age, " Type: " + typeof age);
+console.log(married, " Type: " + typeof married);
+console.log(empty, " Type: " + typeof empty);
