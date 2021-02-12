6f5a9e809510c658ff1f321a8f80a65e5b5ef7da
RakibRahman <md.rr.talukder@gmail.com>
2021-02-12 21:50:55 +0600

    Functions

---
+++ functions-v2.js
@@ -0,0 +1,10 @@
+//((4+2)*2)/2
+
+function math(x, y, z) {
+  var result = ((x + y) * y) / z;
+
+  return result;
+}
+// console.log(Math.trunc(math(2, 5, 20)));
+var result = math(40, 140, 404);
+console.log(result.toFixed(2));
