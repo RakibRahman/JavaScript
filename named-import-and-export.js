40a06ae81f9c5fda595fe253ade620600ea4cd9a
RakibRahman <md.rr.talukder@gmail.com>
2021-02-13 21:09:28 +0600

    Named Import and Export

---
+++ named-import-and-export.js
@@ -0,0 +1,12 @@
+//import must be at top
+/*jshint esversion: 6 */
+import * as Math from "./math.js"; //to import all
+//import { export1 , export2 } from "module-name"; //to import the selected
+console.log(Math.pi);
+console.log(Math.add(90, 10));
+console.log(Math.divide(90, 10));
+console.log(Math.multiply(90, 10));
+console.log(Math.subtract(90, 10));
+
+//console.log(export1(90, 10));
+//console.log(export2(90, 10));
