b2305652bd855727798e00d3734716b0d20b4acc
RakibRahman <md.rr.talukder@gmail.com>
2021-02-13 17:14:00 +0600

    The map() method

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+//The map() method creates a new array with the results of calling a function for every array element.
+
 const arr = [1, 2, 3, 4, 5];
 
 let map = arr.map(function (n) {
@@ -11,3 +13,17 @@ let roots = numbers.map(function (num) {
   return Math.sqrt(num);
 });
 console.log(roots);
+
+var persons = [
+  { firstname: "Rakib", lastname: "Talukder" },
+  { firstname: "Sakib", lastname: "Talukder" },
+  { firstname: "Akib", lastname: "Talukder" },
+];
+
+function getFullname(item) {
+  let fullname = [item.firstname, item.lastname].join(" ");
+  return fullname;
+}
+
+let nameList = persons.map(getFullname);
+console.log(nameList);
