370f3d2ecf268d759dab0ad87ddd61ac96a28865
RakibRahman <md.rr.talukder@gmail.com>
2021-02-13 21:10:02 +0600

    Export file demo

---
+++ math.js
@@ -0,0 +1,13 @@
+export let add = function add(x, y) {
+  return x + y;
+};
+export let subtract = function add(x, y) {
+  return x - y;
+};
+export let multiply = function add(x, y) {
+  return x * y;
+};
+export let divide = function add(x, y) {
+  return x / y;
+};
+export let pi = 3.1415;
