7bd5fe0d0cc64af407ad2355e9b19042a1cdaf39
RakibRahman <md.rr.talukder@gmail.com>
2021-02-13 08:07:54 +0600

    String Methods

---
+++ string-methods.js
@@ -0,0 +1,6 @@
+const email = "md.rr.talukder@gmail.com";
+
+console.log(email.toUpperCase());
+console.log(email.toLowerCase());
+console.log(email.startsWith("md"));
+console.log(email.endsWith("md"));
