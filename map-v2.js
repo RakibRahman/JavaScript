d8f20a4ae126808e55295d1dffc8001165b0029f
RakibRahman <md.rr.talukder@gmail.com>
2021-02-17 23:29:09 +0600

    Map method

---
+++ map-v2.js
@@ -0,0 +1,19 @@
+// The Map method allows you to iterate through all the items in the array to carry out a specific task based on the provided function.
+const films = [
+  { name: "Tenet", year: 2020, rating: 7.5 },
+  { name: "Wonder Woman 1984", year: 2020, rating: 5.4 },
+  { name: "The Karate Kid", year: 1984, rating: 7.3 },
+  { name: "Avengers: Endgame", year: 2019, rating: 8.4 },
+  { name: "The Godfather", year: 1972, rating: 9.2 },
+  { name: "Godzilla", year: 2014, rating: 6.4 },
+  { name: "The Croods: A New Age", year: 2020, rating: 7.0 },
+];
+
+let names = films.map((films) => films.name);
+console.log(names);
+
+let years = films.map((films) => films.year);
+console.log(years);
+
+let total = years.reduce((years, index) => years + index);
+console.log(total);
