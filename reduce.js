4c7782a0383db0b4d7cf7d84dedd8c177de21f93
RakibRahman <md.rr.talukder@gmail.com>
2021-02-17 23:54:43 +0600

    Reduce Method

--- reduce.js
+++ reduce.js
@@ -10,3 +10,44 @@ let reduce2 = arr2.reduce(function (accumulator, current) {
   return accumulator * current;
 });
 console.log(reduce2);
+console.log("---------------------");
+
+const members = [
+  {
+    name: "Rakib",
+    age: 24,
+    year: 1996,
+  },
+  {
+    name: "Rakin",
+    age: 4,
+    year: 2017,
+  },
+  {
+    name: "Labib",
+    age: 12,
+    year: 2008,
+  },
+  {
+    name: "Sadika",
+    age: 11,
+    year: 2009,
+  },
+  {
+    name: "Ruhama",
+    age: 8,
+    year: 2012,
+  },
+  {
+    name: "Faiza",
+    age: 4,
+    year: 2016,
+  },
+];
+
+const total = members.reduce(
+  (currentAge, currentValue) => currentAge + currentValue.age,
+  0
+);
+console.log(`Total Age: ${total}`);
+console.log(`Average Age: ${total / members.length}`);
