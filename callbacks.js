4bae7785f0d331abe964f91b4588d1fad9d264c4
RakibRahman <md.rr.talukder@gmail.com>
2021-02-21 08:48:57 +0600

    Callback Function

--- callbacks.js
+++ callbacks.js
@@ -6,3 +6,17 @@ function callbackExample(name, callback) {
 callbackExample("Rakib", function (name) {
   return "Hello " + name;
 });
+console.log("---------------------");
+
+setTimeout(function () {
+  console.log("inside timer");
+}, 1000);
+
+const outer = (callbackFn) => {
+  console.log("Outer Function");
+  callbackFn();
+};
+
+outer(function inner() {
+  console.log("Inner Callback Function");
+});
