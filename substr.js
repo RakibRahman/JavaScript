34f92454b1a6926dd65adb6d1252b19d4f3141f6
RakibRahman <md.rr.talukder@gmail.com>
2021-02-21 22:22:03 +0600

    The substr() method

---
+++ substr.js
@@ -0,0 +1,4 @@
+//! The substr() method returns a portion of the string, starting at the specified index and extending for a given number of characters afterwards.
+
+const fruit = "Mango";
+console.log(fruit.substring(0, 3));
