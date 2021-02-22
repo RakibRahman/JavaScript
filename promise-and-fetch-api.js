02d3adcf4ad3b426ea7cedfe0005d0d1bb81f0ba
RakibRahman <md.rr.talukder@gmail.com>
2021-02-22 22:34:37 +0600

    Fetch API with Promise

---
+++ promise-and-fetch-api.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+
+const getRandomUsers = (n) => {
+  const fetchRandomUsers = fetch(`https://randomuser.me/api/?results=${n}`);
+  //   console.log(fetchRandomUsers);
+  fetchRandomUsers.then((data) => {
+    data.json().then((randomUsers) => {
+      //console.log(JSON.stringify(randomUsers));
+      console.log(JSON.stringify(randomUsers.results.length));
+      randomUsers.results.forEach((user) => {
+        const { name, gender, email } = user;
+        console.log(`${JSON.stringify(name)} - ${gender} - ${email}`);
+      });
+    });
+  });
+};
+getRandomUsers(1000);
