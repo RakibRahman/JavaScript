ad7f9a639ec346a722c0483edcc0704c8ed7179c
RakibRahman <md.rr.talukder@gmail.com>
2021-02-25 22:33:17 +0600

    The reverse() method

---
+++ array-reverse.js
@@ -0,0 +1,4 @@
+//The reverse() method reverses an array in place. The first array element becomes the last, and the last array element becomes the first.
+const myAwesomeArray = ["e", "d", "c", "b", "a"];
+
+console.log(myAwesomeArray.reverse());
