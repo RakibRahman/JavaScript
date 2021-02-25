1f1c2092ad87e3a67f1b69b16b729320d2a98f3c
RakibRahman <md.rr.talukder@gmail.com>
2021-02-25 21:55:32 +0600

    The sort() method

---
+++ array-sort.js
@@ -0,0 +1,6 @@
+let colors = ["red", "orange", "blue", "lime", "azure"];
+console.log(colors.sort());
+
+const nums = [5, 4, 3, 2, 1, 6, 7, 9, 10, 8];
+console.log(nums.sort((a, b) => a - b));
+console.log(nums.sort((a, b) => b - a));
