1c1594bb3878c9e4857467dd364c4fe4bd4b3945
RakibRahman <md.rr.talukder@gmail.com>
2021-02-25 22:02:56 +0600

    The fill() method

---
+++ array-fill.js
@@ -0,0 +1,7 @@
+//! The fill() method changes all elements in an array to a static value, from a start index (default 0) to an end index (default array.length). It returns the modified array.
+
+const arr = [1, 2, 3, 4];
+
+// fill with "value 0" from position 2 until position 4
+
+console.log(arr.fill(0, 2, 4));
