8825071ed1e73c94ad437f5ef5a5a2803b33bab1
RakibRahman <md.rr.talukder@gmail.com>
2021-02-28 00:52:17 +0600

    Switch statements

--- switch.js
+++ switch.js
@@ -22,13 +22,31 @@ switch (true) {
   default:
     console.log(person.name, "Not a Adult");
 }
+
+var day;
 switch (new Date().getDay()) {
-  case 13:
-    console.log("Today is Saturday");
+  case 0:
+    day = "Sunday";
     break;
-  case 12:
-    console.log("Today is Sunday");
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
     break;
   default:
-    console.log("Unknown");
+    day = "unknown";
 }
+console.log(`Today is ${day}`);
