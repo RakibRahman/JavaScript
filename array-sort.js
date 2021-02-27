f9115a799868db9ce404e124581d453e3d36f48f
RakibRahman <md.rr.talukder@gmail.com>
2021-02-28 00:43:54 +0600

    Sort Method with The Compare Function

--- array-sort.js
+++ array-sort.js
@@ -2,5 +2,13 @@ let colors = ["red", "orange", "blue", "lime", "azure"];
 console.log(colors.sort());
 
 const nums = [5, 4, 3, 2, 1, 6, 7, 9, 10, 8];
-console.log(nums.sort((a, b) => a - b));
+console.log(nums.sort((a, b) => a - b)); //The Compare Function
 console.log(nums.sort((a, b) => b - a));
+
+const points = [40, 100, 1, 5, 25, 10];
+const random = points.sort((a, b) => 0.5 - Math.random());
+console.log(random);
+
+random.forEach((element) => {
+  console.log(element);
+});
