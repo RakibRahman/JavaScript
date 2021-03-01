e0a499c316d20e8281cfefb1ea6f070c3f5fe9e2
RakibRahman <md.rr.talukder@gmail.com>
2021-03-01 22:34:30 +0600

    Async Await

--- async-await.js
+++ async-await.js
@@ -1,4 +1,7 @@
+const fetch = require("node-fetch");
+
 const { promises } = require("fs");
+const { resolve } = require("path");
 
 async function logName(name) {
   console.log(name);
@@ -17,3 +20,21 @@ async function logName(name) {
 logName("Rakib").then((res) => {
   console.log(`result from async function: ${res}`);
 });
+
+console.log("---------------------");
+
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("resolved");
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log("Calling");
+
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+}
+asyncCall();
