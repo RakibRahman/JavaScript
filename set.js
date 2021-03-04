e863db2157e4c9d23f851d8d0acd11d3c623d0ed
RakibRahman <md.rr.talukder@gmail.com>
2021-03-04 23:15:11 +0600

    The Set() Object

--- set.js
+++ set.js
@@ -12,16 +12,26 @@ const languages = [
 
 const setOflanguages = new Set(languages);
 console.log(setOflanguages);
+//Output: Set(4) { 'English', 'Finnish', 'Bangla', 'Tamil' }
 
 //iterate elements
-for (let x of setOflanguages) {
-  console.log(x);
+for (let items of setOflanguages) {
+  console.log(items);
 }
+/*
+Output:
+English
+Finnish
+Bangla;
+Tamil;
+*/
 console.log("---------------------");
 
 //Adding an element to a set
 
 const names = new Set();
+console.log(names);
+//output: Set(0) {}
 
 console.log(names.size);
 names.add("Rakib");
@@ -30,15 +40,18 @@ names.add("Hakim");
 names.add("Sakir");
 names.add("Sakir2");
 console.log(names.size);
+//output: 5
 console.log(names);
-console.log(names.delete("Sakir2")); //delete element
+console.log(names.delete("Sakir2"));
+//Output: Set(4) { 'Rakib', 'Zakir', 'Hakim', 'Sakir' }
 console.log(names);
 
 //Checking an element in the set
 
 console.log(names.has("Rakib"));
-
+//Output: true
 //Clearing the set
 
 names.clear();
 console.log(names);
+//Output: Set(0) {}
