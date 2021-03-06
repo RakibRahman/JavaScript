554c7043c9f0f229879924c782af4bfd4a4b2819
RakibRahman <md.rr.talukder@gmail.com>
2021-03-07 00:28:03 +0600

    String Methods

--- string-methods.js
+++ string-methods.js
@@ -24,3 +24,32 @@ let str = "Mango,Banana,Jackfruit,Banana";
 let newStr = str.replace("Banana", "Berry");
 
 console.log(newStr);
+
+console.log("---------------------");
+
+//!The charCodeAt() Method
+//returns unicode of specified index
+let jutsu = "shinra tensei";
+let lastChar = jutsu.length - 1;
+console.log(jutsu.charCodeAt(lastChar));
+
+console.log("---------------------");
+
+// !The Concat() Method
+let fName = "Rakibur";
+let lName = "Talukder";
+let fullName = fName.concat(" ", lName);
+console.log(fullName);
+
+//! The trim() Method
+//removes whitespace from right and left.
+const person = "      Md.  Rakibur  Rahman   Talukder ";
+console.log(person);
+console.log(person.trim());
+
+//The split() method
+//!convert string to array
+let letters = "r,a,k,i,b";
+console.log(letters.split(",")); //split on commas
+console.log(letters.split(" ")); //split on spaces
+console.log(letters.split(" | ")); //split on pipe
