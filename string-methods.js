41dcd38e2dc8253ff3dbc44c13f30357136a0f4a
RakibRahman <md.rr.talukder@gmail.com>
2021-03-06 22:02:19 +0600

    String Methods

--- string-methods.js
+++ string-methods.js
@@ -1,6 +1,18 @@
 const email = "md.rr.talukder@gmail.com";
-
+const givenName = "Md. Rakibur Rahman Talukder";
 console.log(email.toUpperCase());
 console.log(email.toLowerCase());
 console.log(email.startsWith("md"));
 console.log(email.endsWith("md"));
+console.log(`Length of ${email} is ${email.length}`);
+
+console.log("---------------------");
+console.log(givenName.indexOf("Rakib"));
+console.log(givenName.indexOf("Ra", 6));
+
+console.log("---------------------");
+
+//! The Search Method
+
+const sData = givenName.search("Rakib");
+console.log(sData);
