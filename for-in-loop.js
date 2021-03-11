95b76ae708679e120dddb6040b846a3d0df04c09
RakibRahman <md.rr.talukder@gmail.com>
2021-03-11 17:58:54 +0600

    for...in statement

--- for-in-loop.js
+++ for-in-loop.js
@@ -3,3 +3,18 @@ const object = { a: 1, b: 2, c: 3 };
 for (const items in object) {
   console.log(`${items} : ${object[items]}`);
 }
+
+const name = {
+  firstName: "Rakin",
+  lastName: "Amin",
+  age: 4,
+};
+console.log(name);
+
+let text = "";
+
+for (let info in name) {
+  //console.log(`${info}`);
+  text = info + " : " + name[info];
+  console.log(text);
+}
