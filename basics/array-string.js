3a56fa2db4456588f34c606e56929a45899bfdfa
RakibRahman <md.rr.talukder@gmail.com>
2021-03-12 23:05:14 +0600

    Split & Join String Methods

---
+++ basics/array-string.js
@@ -0,0 +1,9 @@
+let string = "Kill Me Heal Me";
+let arr = string.split(" ");
+console.log(arr);
+console.log(arr[2]);
+
+let demo = arr.join(".");
+console.log(demo);
+
+console.log("This World Shall Know Pain".split(" "));
