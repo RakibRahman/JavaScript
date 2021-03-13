9180a4d47d91cf06a6a08b464796d049b3082b9f
RakibRahman <md.rr.talukder@gmail.com>
2021-03-13 20:57:50 +0600

    Split() Method

---
+++ basics/string-split.js
@@ -0,0 +1,7 @@
+let quote = "Kill Me Heal Me";
+let words = quote.split(" ");
+let characters = quote.split("");
+let l = quote.split("l");
+console.log("Total words in quote:", words.length);
+console.log("Total characters in quote:", characters.length);
+console.log("Total 'L' in quote:", l.length - 1);
