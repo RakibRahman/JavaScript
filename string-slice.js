a2af981bfbfb546ef0b98a0cff4058d9ca625bcd
RakibRahman <md.rr.talukder@gmail.com>
2021-03-13 12:11:29 +0600

    Slice() Method

--- string-slice.js
+++ string-slice.js
@@ -14,3 +14,18 @@ const short = myStatus.slice(0, 200);
 
 console.log(myStatus.length);
 console.log(short.length);
+
+console.log("---------------------");
+
+let quote =
+  "welcome to the World War III , Kill the Master and items and items of items";
+let firstChar = quote.indexOf("World War III");
+
+if (firstChar !== -1) {
+  quote =
+    quote.slice(0, firstChar) +
+    "The Third World War" +
+    quote.slice(firstChar + 13);
+}
+
+console.log(quote);
