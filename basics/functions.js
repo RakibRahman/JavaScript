bac9f087be51bf38be54a484ebfcc3eeda7d362b
RakibRahman <md.rr.talukder@gmail.com>
2021-03-15 00:27:11 +0600

    Functions

---
+++ basics/functions.js
@@ -0,0 +1,14 @@
+function getName(name) {
+  console.log("Hello " + name);
+}
+getName("Rakib");
+
+function isEven(number) {
+  if (number % 2 == 0) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isEven(12));
+console.log(isEven(91));
