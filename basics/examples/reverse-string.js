ce78cae246ad85e300f601b418b733a31fb62d79
RakibRahman <md.rr.talukder@gmail.com>
2021-03-15 00:35:22 +0600

    Reverse a string

--- basics/examples/reverse-string.js
+++ basics/examples/reverse-string.js
@@ -1,3 +1,14 @@
 let str = "Rakib Talukder";
 let reverseStr = str.split("").reverse().join("");
 console.log(reverseStr);
+
+console.log("---------------------");
+
+//? Reverse string with functions
+
+const reverseString = (string) => {
+  const reverse = string.split("").reverse().join("");
+  console.log(reverse);
+};
+
+reverseString("hello world");
