4fc952eff57c9ff50d2f5180abd0486515d4d513
RakibRahman <md.rr.talukder@gmail.com>
2021-03-15 01:03:08 +0600

    Functions

--- basics/functions.js
+++ basics/functions.js
@@ -13,3 +13,13 @@ function isEven(number) {
 
 console.log(isEven(12));
 console.log(isEven(91));
+
+console.log("---------------------");
+
+//function with unlimited arguments
+
+function addNames(...params) {
+  console.log("Top rated players: ", params.length);
+  console.log("Top rated players: ", params);
+}
+addNames("Rakib", "Zakir");
