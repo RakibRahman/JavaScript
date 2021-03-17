b6dceb3c0aa7a45f94901140cbf18669f4896cb3
RakibRahman <md.rr.talukder@gmail.com>
2021-03-18 02:41:34 +0600

    map() Method

---
+++ arrayMethods/map.js
@@ -0,0 +1,18 @@
+const items = [
+  { id: "🍔", name: "Super Burger", price: 150 },
+  { id: "🥩", name: "Super Meat", price: 450 },
+  { id: "🍜", name: "Super Noodles", price: 180 },
+];
+//! update the price
+
+const halfPrice = items.map((item) => {
+  if (item.id === "🥩") {
+    return {
+      ...item,
+      price: item.price / 2,
+      //? using spread operator to return a new object ,instead of mutating the existing object.
+    };
+  }
+  return item;
+});
+console.log(halfPrice);
