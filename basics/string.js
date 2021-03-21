42b3d01446e95ad1a5d6dced774b6c6cf7d800cd
RakibRahman <md.rr.talukder@gmail.com>
2021-03-21 17:47:03 +0600

    string

---
+++ basics/string.js
@@ -0,0 +1,18 @@
+let a = "Rakib";
+let b = "Talukder";
+
+if (a == b) {
+  var identity = "Same";
+  console.log(identity);
+} else {
+  console.log("Not Same");
+}
+
+console.log("---------------------");
+
+if (a !== b) {
+  var identity = "Same";
+  console.log(identity);
+} else {
+  console.log("Not Same");
+}
