32a8033a115ddd0789208cd969c968bc711fb171
RakibRahman <md.rr.talukder@gmail.com>
2021-03-22 21:56:00 +0600

    Switch Statement

---
+++ basics/switch.js
@@ -0,0 +1,20 @@
+var idNo = 49;
+switch (idNo) {
+  case 45:
+    console.log("Zakir");
+    break;
+  case 46:
+    console.log("Apple");
+    break;
+  case 47:
+    console.log("Ash");
+    break;
+  case 48:
+    console.log("hasib");
+    break;
+  case 49:
+    console.log("Rakib");
+    break;
+  default:
+    console.log("Not in the list");
+}
