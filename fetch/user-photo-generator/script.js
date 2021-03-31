2e81da72a0c368785da707006713e1addf63539e
RakibRahman <md.rr.talukder@gmail.com>
2021-03-31 22:39:27 +0600

    User Photo Generator

---
+++ fetch/user-photo-generator/script.js
@@ -0,0 +1,25 @@
+const output = document.getElementById("output");
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+
+const url = "https://randomuser.me/api/";
+
+const fetchUser = () => {
+  let tempURL = url + "?results=" + input.value;
+
+  const randomUser = fetch(tempURL);
+  randomUser
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      const user = data.results;
+      console.log(data);
+      output.innerText = "";
+      user.forEach((item) => {
+        output.innerHTML += "<img src='" + item.picture.large + "'" + "</br>";
+      });
+    });
+};
+btn.addEventListener("click", fetchUser);
