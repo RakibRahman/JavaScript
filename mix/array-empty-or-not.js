012797bf0ae6307a2552ccad69f489c29f391c15
RakibRahman <md.rr.talukder@gmail.com>
2021-04-04 01:29:01 +0600

    Empty array or not

---
+++ mix/array-empty-or-not.js
@@ -0,0 +1,17 @@
+let array1 = [1, 2, 3, 4, 5];
+let array2 = [];
+let array3 = null;
+let array4 = undefined;
+
+array1 && array1.length
+  ? console.log("Not a Empty Array")
+  : console.log("Empty Array");
+array2 && array2.length
+  ? console.log("Not a Empty Array")
+  : console.log("Empty Array");
+array3 && array3.length
+  ? console.log("Not a Empty Array")
+  : console.log("Empty Array");
+array4 && array4.length
+  ? console.log("Not a Empty Array")
+  : console.log("Empty Array");
