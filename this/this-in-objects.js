99ee1f0874b71eea38959074e052cbbc97be78db
RakibRahman <md.rr.talukder@gmail.com>
2021-04-12 08:18:40 +0600

    this in objects

--- this/this-in-objects.js
+++ this/this-in-objects.js
@@ -28,3 +28,35 @@ let message = {
 };
 
 message.email.msg();
+
+console.log("---------------------");
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    console.log(`Current Step: ${this.step}`); //shows the current step
+    return this;
+  },
+};
+
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.up();
+// ladder.showStep();
+console.log("---------------------");
+
+//! make calls chainable
+// ladder.up().up().down().showStep();
+console.log("---------------------");
+
+ladder.up().up().down().up().down().showStep();
+ladder.up().up().showStep();
