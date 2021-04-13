1fbc0c8d467f4b9d1e60eb2e3fc40b4c781d1b03
RakibRahman <md.rr.talukder@gmail.com>
2021-04-13 15:12:39 +0600

    Constructor Function

--- functions/constructor-function.js
+++ functions/constructor-function.js
@@ -15,3 +15,40 @@ counter1.incrementCounter();
 counter1.incrementCounter();
 counter1.incrementCounter();
 counter1.decrementCounter();
+
+console.log("---------------------");
+
+function Person() {
+  //define properties
+  this.name = "Joker";
+  this.age = 25;
+
+  this.getName = function () {
+    // "this" here refers to the "Person" constructor.
+    // "this.name" is like "Person.name".
+    return `Hello, my name is ${this.name}.`;
+  };
+}
+
+//Create object with Person constructor
+
+const personOne = new Person();
+const personTwo = new Person();
+
+console.log(personOne.name);
+console.log(personOne.getName());
+
+personOne.gender = "Male";
+console.log(personOne.gender);
+console.log(personTwo.gender); // gender only exists only on personOne
+
+personTwo.height = 1.8;
+console.log("Height:", personTwo.height);
+console.log("Height:", personOne.height);
+
+const personThree = new Person();
+
+personThree.getAge = function () {
+  return `Age: ${this.age}`;
+};
+console.log(personThree.getAge());
