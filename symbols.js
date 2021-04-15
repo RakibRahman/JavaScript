35a4c2c74d3a09a48d9750a78bce87674051f455
RakibRahman <md.rr.talukder@gmail.com>
2021-04-15 15:14:11 +0600

    Symbol

--- symbols.js
+++ symbols.js
@@ -47,3 +47,7 @@ iterable1[Symbol.iterator] = function* () {
   yield 3;
 };
 console.log(...iterable1);
+
+console.log(typeof Symbol() === "symbol");
+console.log(typeof Symbol("foo") === "symbol");
+console.log(typeof Symbol.iterator === "symbol");
